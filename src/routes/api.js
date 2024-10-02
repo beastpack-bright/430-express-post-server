4b37d6f95d07c0fe02625801bb66b00043a4cc18
Elizabeth Sanabria <143511550+beastpack-bright@users.noreply.github.com>
2024-10-02 12:32:54 -0400

    aaa

--- src/routes/api.js
+++ src/routes/api.js
@@ -35,4 +35,39 @@ router.get('/timeJSON', (req, res) => {
     content: new Date(),
   });
 });
+const getHootById = (id) => {
+  const hoot = hoots.find((h) => h.id === id);
+  return hoot;
+};
+
+const deleteHootById = (id) => {
+  const hoot = getHootById(id);
+  if (!hoot) return null;
+  const index = hoots.indexOf(hoot);
+  hoots.splice(index, 1);
+  return hoot;
+};
+router.delete('/deleteHoot/:id([0-9,a-z,A-Z,-]{36})', (req, res) => {
+  const hoot = deleteHootById(req.params.id);
+  if (!hoot) {
+    const error = `id: '${req.params.id}' not found`;
+    res.status(404).send({ error });
+  } else {
+    res.json(hoot);
+  }
+});
+router.put('/updateHoot/:id([0-9,a-z,A-Z,-]{36})', (req, res) => {
+  const hoot = getHootById(req.params.id);
+  if (!hoot) {
+    const error = `id: '${req.params.id}' not found`;
+    res.status(404).send({ error });
+  } else {
+    const content = req.body && req.body.content
+      ? req.body.content
+      : 'No req.body or req.body.content found!';
+    hoot.content = content;
+    hoot.updatedAt = new Date();
+    res.json(hoot);
+  }
+});
 module.exports = router;
