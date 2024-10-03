01118d18ee3485b1cb5a1cdfdc55a7fd630a8d7f
Elizabeth Sanabria <143511550+beastpack-bright@users.noreply.github.com>
2024-10-03 17:21:30 -0400

    Updated, New Final

---
+++ src/db.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const quotesPath = path.resolve(__dirname, 'data/quotes-data.json');
+const jsonString = fs.readFileSync(quotesPath);
+const data = JSON.parse(jsonString);
+const { quotes } = data; 
+
+// PUBLIC METHODS
+const getAllQuotes = () => quotes;
+
+const randomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
+
+const recentQuote = () => quotes[quotes.length - 1];
+
+const getQuoteById = (id) => quotes.find(q => q.id === id);
+
+module.exports = { getAllQuotes, randomQuote, recentQuote, getQuoteById };
